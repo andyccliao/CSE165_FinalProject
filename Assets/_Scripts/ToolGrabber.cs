cd2f31b32d9c54fdf5a1208b0148ba4b11606cc2
acliao <acliao@ucsd.edu>
2018-03-13 21:29:25 -0700

    added Tool and toolbelt framework, nothing coded yet though

---
+++ Assets/_Scripts/ToolGrabber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolGrabber : OVRGrabber {
+
+    protected ToolScript lastTool = null;
+
+    protected override void GrabBegin()
+    {
+        base.GrabBegin();
+
+        if (lastTool != null) {
+            lastTool.enabled = false;
+        }
+
+        lastTool = m_grabbedObj.GetComponent<ToolScript>();
+
+        if (lastTool != null) {
+            lastTool.enabled = true;
+        }
+    }
+}
